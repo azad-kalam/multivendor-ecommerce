985589f7b108cd3e775b82d258c0199bf6a6a75f
Azad Kalam <azad.kalam4772@gmail.com>
2026-03-04 23:25:37 +0600

    modified admin route profile

--- routes/profile.php
+++ routes/profile.php
@@ -1,20 +1,19 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RoleMiddleware;
-use Illuminate\Support\Facades\Artisan;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
 
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-$adminMiddleware = (['auth', 'verified', RoleMiddleware::class . ':admin']);
+$adminMiddleware = ['auth', 'verified', RoleMiddleware::class . ':admin'];
+
 Route::middleware($adminMiddleware)
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
-        Route::resource('profile', AdminProfileController::class)->names('profile');
+        Route::get('profile', [AdminProfileController::class, 'index'])->name('profile.index');
+
+        Route::get('profile/edit', [AdminProfileController::class, 'edit'])->name('profile.edit');
+
+        Route::put('profile', [AdminProfileController::class, 'update'])->name('profile.update');
     });
