902bb26d40e66caa9443dafe51ab0e1e925ed031
Azad Kalam <azad.kalam4772@gmail.com>
2026-03-09 22:15:57 +0600

    add route and controller, profile and profileController of vendor

--- routes/profile.php
+++ routes/profile.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RoleMiddleware;
 use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
+use App\Http\Controllers\Vendor\ProfileController as VendorProfileController;
 
 Route::middleware(['auth', 'verified', RoleMiddleware::class . ':admin'])
     ->prefix('admin')
@@ -20,4 +21,22 @@ Route::middleware(['auth', 'verified', RoleMiddleware::class . ':admin'])
 
         Route::get('/profile/change-password', [AdminProfileController::class, 'changePassword'])->name('profile.change-password');
         Route::post('/profile/change-password', [AdminProfileController::class, 'updatePassword'])->name('profile.update-password');
+    });
+
+    Route::middleware(['auth', 'verified', RoleMiddleware::class . ':vendor'])
+    ->prefix('vendor')
+    ->name('vendor.')
+    ->group(function () {
+
+        Route::get('/profile', [VendorProfileController::class, 'index'])
+            ->name('profile.index');
+
+        Route::get('/profile/edit', [VendorProfileController::class, 'edit'])
+            ->name('profile.edit');
+
+        Route::put('/profile/update', [VendorProfileController::class, 'update'])
+            ->name('profile.update');
+
+        Route::get('/profile/change-password', [VendorProfileController::class, 'changePassword'])->name('profile.change-password');
+        Route::post('/profile/change-password', [VendorProfileController::class, 'updatePassword'])->name('profile.update-password');
     });
