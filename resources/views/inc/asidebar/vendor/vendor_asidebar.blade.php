8a9ab8dd7436d57f35aeddac4d3b54c6c5f951fc
Azad Kalam <azad.kalam4772@gmail.com>
2026-03-11 21:55:26 +0600

    modified views vendor asidebar

--- resources/views/inc/asidebar/vendor/vendor_asidebar.blade.php
+++ resources/views/inc/asidebar/vendor/vendor_asidebar.blade.php
@@ -7,7 +7,6 @@
             </a>
         </li>
 
-        {{-- <li class="nav-item"> --}}
         <li class="nav-item {{ Request::is('vendor/details*') ? 'active' : '' }}">
             <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
                 <i class="fa-solid fa-user"></i>
@@ -23,7 +22,6 @@
             </ul>
         </li>
 
-        {{-- product toggle start here --}}
         <li class="nav-item {{ Request::is('vendor/products*') ? 'active' : '' }}">
             <a class="nav-link collapsed" data-bs-target="#vendor-products" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-layout-text-window-reverse"></i>
@@ -43,8 +41,6 @@
                 </li>
             </ul>
         </li>
-        {{-- product toggle end here --}}
-
 
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
@@ -72,25 +68,31 @@
         <li class="nav-heading">Pages</li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="users-profile.html">
+            <a class="nav-link collapsed" href="{{ route('vendor.profile.index') }}">
                 <i class="bi bi-person"></i>
                 <span>Profile</span>
             </a>
-        </li><!-- End Profile Page Nav -->
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ route('vendor.contact.index') }}">
+                <i class="fa fa-envelope"></i>
+                <span>Contact</span>
+            </a>
+        </li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="pages-register.html">
                 <i class="bi bi-card-list"></i>
                 <span>Register</span>
             </a>
-        </li><!-- End Register Page Nav -->
+        </li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="pages-login.html">
                 <i class="bi bi-box-arrow-in-right"></i>
                 <span>Login</span>
             </a>
-        </li><!-- End Login Page Nav -->
-
+        </li>
     </ul>
 </aside>
