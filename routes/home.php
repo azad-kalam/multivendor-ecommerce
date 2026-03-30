92cc6279c7c8913a1eabdf9922f5e7185e06ba21
Azad Kalam <azad.kalam4772@gmail.com>
2026-03-30 22:25:51 +0600

    add routes method productShowByCategory of home

--- routes/home.php
+++ routes/home.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Home\HomeController;
 
 // home page access all visitors no need login or registration
 Route::get('/', [HomeController::class, 'index'])->name('homepage.index');
+Route::get('/category/{id}', [HomeController::class, 'productShowByCategory'])->name('homepage.product_by_category');
