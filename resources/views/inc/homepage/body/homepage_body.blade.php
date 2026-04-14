19821e4c2d15608abd3ac5089943babcd5c5b086
Azad Kalam <azad.kalam4772@gmail.com>
2026-04-14 22:01:47 +0600

    add views sub category products 6

--- resources/views/inc/homepage/body/homepage_body.blade.php
+++ resources/views/inc/homepage/body/homepage_body.blade.php
@@ -356,6 +356,7 @@
   <h1>sub category products 3</h1>
   <h1>sub category products 4</h1>
   <h1>sub category products 5</h1>
+  <h1>sub category products 6</h1>
   <!-- sub category wise products end here -->
 
   <!-- our product section start here -->
