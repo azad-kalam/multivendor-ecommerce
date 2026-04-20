139bb4f914a58c2a62d214784377b884bd264863
Azad Kalam <azad.kalam4772@gmail.com>
2026-04-20 22:36:18 +0600

    sub category products 13

--- resources/views/inc/homepage/body/homepage_body.blade.php
+++ resources/views/inc/homepage/body/homepage_body.blade.php
@@ -363,6 +363,8 @@
   <h1>sub category products 10</h1>
   <h1>sub category products 11</h1>
   <h1>sub category products 12</h1>
+  <h1>sub category products 13</h1>
+
   <!-- sub category wise products end here -->
 
   <!-- our product section start here -->
