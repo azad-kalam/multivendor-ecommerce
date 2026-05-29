2f15bb44b20dca665cae55cdf68a3a9b508730e4
Azad Kalam <azad.kalam4772@gmail.com>
2026-05-29 14:04:52 +0600

    add sub category products 61

--- resources/views/inc/homepage/body/homepage_body.blade.php
+++ resources/views/inc/homepage/body/homepage_body.blade.php
@@ -411,6 +411,7 @@
   <h1>sub category products 58</h1>
   <h1>sub category products 59</h1>
   <h1>sub category products 60</h1>
+  <h1>sub category products 61</h1>
   <!-- sub category wise products end here -->
 
   <!-- our product section start here -->
