b4a0f9842a0ee959700b9119755cb2bdd6a05b3f
Azad Kalam <azad.kalam4772@gmail.com>
2026-08-29 20:22:52 +0600

    modified cart routes

--- routes/cart.php
+++ routes/cart.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\FrontEnd\CartController;
 
 Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.store');
 Route::post('shopping-cart', [CartController::class, 'show'])->name('frontend.carts.show');
+Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.delete');
 
 
