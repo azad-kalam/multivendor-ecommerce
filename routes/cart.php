5a705a7989a29eb6bc1b0d5107c7b49158304c50
Azad Kalam <azad.kalam4772@gmail.com>
2026-08-30 20:37:24 +0600

    add cart index route

--- routes/cart.php
+++ routes/cart.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontEnd\CartController;
 
+Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.index');
 Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.store');
 Route::post('shopping-cart', [CartController::class, 'show'])->name('frontend.carts.show');
 Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.delete');
