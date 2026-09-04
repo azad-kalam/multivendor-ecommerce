b931e399730ceb144732d28affb2d71c87f221a5
Azad Kalam <azad.kalam4772@gmail.com>
2026-09-04 21:50:37 +0600

    cart route

--- routes/cart.php
+++ routes/cart.php
@@ -4,12 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontEnd\CartController;
 
 Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.index');
+Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.create');
 Route::post('add-to-cart', [CartController::class, 'store'])->name('frontend.carts.store');
 Route::post('shopping-cart', [CartController::class, 'show'])->name('frontend.carts.show');
 Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.update');
 Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.delete');
-Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.ajax');
-Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.ajax-frontend');
-Route::post('{cart_id}', [CartController::class, 'delete'])->name('frontend.carts.count');
-
-
