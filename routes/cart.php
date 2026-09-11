7a9bc943ea655379a77536f01d4aa7d5d2cfcf76
Azad Kalam <azad.kalam4772@gmail.com>
2026-09-11 23:10:04 +0600

    add shopping-cart route

--- routes/cart.php
+++ routes/cart.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\FrontEnd\CartController;
 
 Route::prefix('cart')->group(function () {
     Route::resource('/', CartController::class)->names('frontend.carts');
+     Route::get('/shopping-cart', [CartController::class, 'ajax_shopping_cart'])->name('shopping-cart');
 });
