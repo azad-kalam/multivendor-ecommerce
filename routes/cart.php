86e8dcb2574ac69bd21e422880f47beb0e5d591f
Azad Kalam <azad.kalam4772@gmail.com>
2026-09-26 21:20:59 +0600

    modified cart routes

--- routes/cart.php
+++ routes/cart.php
@@ -4,12 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontEnd\CartController;
 
 Route::prefix('cart')->name('frontend.carts.')->group(function () {
-
     Route::get('/', [CartController::class, 'index'])->name('index');
-
     Route::post('/', [CartController::class, 'store'])->name('store');
-
-    Route::delete('/{id}', [CartController::class, 'destroy'])->name('destroy');
-
-    Route::get('/shopping-cart', [CartController::class, 'ajax_cart_and_summary'])->name('shopping-cart');
+    Route::patch('/{cartId}', [CartController::class, 'update'])->name('update');
+    Route::delete('/{cartId}', [CartController::class, 'destroy'])->name('destroy');
+    Route::get('/shopping-cart', [CartController::class, 'render_cart_and_summary'])->name('shopping-cart');
 });
